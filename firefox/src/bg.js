6f41395d0f68f524d9f4f32de9a4bb9a2b78ad4f
henrikdetjen <zugang@gmx.eu>
2016-08-31 00:47:45 +0200

    ...

--- firefox/src/bg.js
+++
@@ -1,9 +0,0 @@
-function notify(msg) {
-    var n = chrome.notifications.create({
-        "type": "basic",
-        "iconUrl": chrome.extension.getURL("unlock.png"),
-        "title": msg.title,
-        "message": msg.content
-    });
-}
-chrome.runtime.onMessage.addListener(notify);
